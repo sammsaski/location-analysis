91291208ee62e6acdfbac58e41a17c363d86c505
sammsaski <samuel.sasaki@gmail.com>
2022-04-09 18:11:16 -0700

    Application can flip camera, take a picture, and display the image uri in an alert

--- App.js
+++ App.js
@@ -1,17 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Dimensions, Alert } from 'react-native';
+import { Camera } from 'expo-camera';
 import HorizontalScrollView from './components/HorizontalScrollView';
+import { ImageBackground } from 'react-native-web';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  // camera ref to access Camera
+  const cameraRef = useRef(null);
+
+  /**
+   * called to take the photo
+   */
+  const takePhoto = async () => {
+    if (cameraRef) {
+      console.log("in take picture");
+      try {
+        let photo = await cameraRef.current.takePictureAsync({
+          allowsEditing: true,
+          aspect: [4,3],
+          quality: 1,
+        });
+        return photo
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
-    // <View style={styles.container}>
-    //   <Text>Open up App.js to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
-    <HorizontalScrollView>
-      <Text>Open up App.js to start working on your app!</Text>
-    </HorizontalScrollView>
+    <View style={styles.container}>
+      <Camera style={styles.camera} type={type} ref={cameraRef}>
+        <View style={styles.flipButtonContainer}>
+          <TouchableOpacity
+            style={styles.flipButton}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}>
+            <Text style={styles.text}> Flip </Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.cameraCaptureButtonContainer}>
+          <TouchableOpacity
+           style={styles.cameraCaptureButton}
+           onPress={async () => {
+             const r = await takePhoto();
+             Alert.alert("DEBUG", JSON.stringify(r))
+            }}
+           >
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    </View>
   );
 }
 
@@ -22,4 +82,47 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  cameraCaptureButtonContainer: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    margin: 20
+  },
+
+  cameraCaptureButton: {
+    flex: 0.5,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+    width: Dimensions.get('window').width / 5,
+    height: Dimensions.get('window').width / 5,
+    borderRadius: 50,
+    borderWidth: 3,
+    borderColor: '#4169e1'
+  },
+
+  flipButtonContainer: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    flexDirection: 'column',
+    margin: 20
+  },
+
+  flipButton: {
+    flex: 0.1,
+    alignSelf: 'flex-end',
+    alignItems: 'center'
+  },
+
+  text: {
+    fontSize: 18,
+    color: 'red'
+  },
+
+  camera: {
+    flex: 1,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height
+  }
 });
